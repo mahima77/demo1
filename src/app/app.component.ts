d42ca680ff878f9ed931fd63c874b46bb9a0c299
mahima <mahi77buley@gmail.com>
2021-08-13 20:10:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl,FormArray, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formbuilder';
+ 
+  disname:boolean = true;
+  skillsForm: FormGroup;
+ 
+  constructor(private fb:FormBuilder) {
+ 
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([ new FormGroup({
+                              name: new FormControl(''),
+                              assignedname: new FormControl(''),
+                              description: new FormControl(''),
+                              summary: new FormControl(''),
+                         })]) ,
+      address: new FormGroup({
+        country: new FormControl(''),
+        city: new FormControl('')
+      })
+    });
+  
+  }
+ 
+  get skills() : FormArray {
+    return this.skillsForm.get("skills") as FormArray
+  }
+ 
+  newSkill(): FormGroup {
+    return this.fb.group({
+     name: '',
+                              assignedname: '',
+                              description: '',
+                              summary:''
+    })
+  }
+ 
+  addSkills() {
+    this.skills.push(this.newSkill());
+  }
+ 
+  removeSkill(i:number) {
+    this.skills.removeAt(i);
+  }
+ 
+  onSubmit() {
+    console.log(this.skillsForm.value);
+  }
+
+   dissablefield(a){
+     this.disname = a;
+     console.log( this.disname)
+     
+  }
+ 
 }
+ 
+ 
+export class country {
+  id: string;
+  name: string;
+ 
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+ 
